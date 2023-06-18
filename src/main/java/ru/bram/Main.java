29fb3be11e814b0774b1b66cb51d936d130f1c4a
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-06-18 14:11:20 +0300

    Slow commit

--- src/main/java/ru/bram/Main.java
+++ src/main/java/ru/bram/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public  static void main(String[] args){
         System.out.println("Hello word");
     }
+    private  static String getMasegge(){
+        return "Hello bram";
+    }
 }
